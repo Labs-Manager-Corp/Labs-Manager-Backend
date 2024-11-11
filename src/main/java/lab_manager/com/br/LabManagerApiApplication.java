07e938a0b1c524ddd41d493be640707804936f7a
SirOtaiv <rauotavio7@gmail.com>
2024-11-11 20:00:12 -0300

    First Commit

---
+++ src/main/java/lab_manager/com/br/LabManagerApiApplication.java
@@ -0,0 +1,13 @@
+package lab_manager.com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab_manager/com/br/LabManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lab_manager.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
